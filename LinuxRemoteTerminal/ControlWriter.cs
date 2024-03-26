ed978ee9a11160fc199992371ab340eaa46c295e
kvetinskystepan <stepan.kvetinsky@gmail.com>
2024-03-26 22:11:15 +0100

    d

---
+++ LinuxRemoteTerminal/ControlWriter.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LinuxRemoteTerminal
+{
+    public class ControlWriter : TextWriter
+    {
+        private Control richTextBox;
+        public ControlWriter(Control richTextBox)
+        {
+            this.richTextBox = richTextBox;
+        }
+
+        public override void Write(char value)
+        {
+            richTextBox.Invoke((MethodInvoker)delegate
+            {
+                richTextBox.Text += value.ToString();
+            });
+        }
+
+        public override void Write(string value)
+        {
+            richTextBox.Invoke((MethodInvoker)delegate
+            {
+                richTextBox.Text += (value);
+            });
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
